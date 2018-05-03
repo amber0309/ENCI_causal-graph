cd419b6d5a9a8693b1f04941babe7d5b7a67f317
Seth <ambermit@hotmail.com>
2018-05-03 09:26:18 +0800

    Update median_dist.m

--- code/kernel/median_dist.m
+++ code/kernel/median_dist.m
@@ -5,7 +5,7 @@ tempMM = zeros(1,num_of_feature); %store median of each column
 
 for t = 1:num_of_feature
     dist = pdist2(S1(:,t), S2(:,t));
-    
+    dist(dist==0) = [];
     tempMM(1,t) = median(dist(:));
 end
 
